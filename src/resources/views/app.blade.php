b7c8968eca8fc93a15349590863972db2f14e6c8
MulhamShaheen <mulhamshaheen.ms@gmail.com>
2023-01-29 20:51:01 +0500

    Blade fixed minor errors

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -1,8 +1,9 @@
+<!DOCTYPE html>
+<html>
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0">
 
-    <!-- @routes -->
     <link href="{{ asset(mix('css/app.css')) }}" rel="stylesheet">
     <script src="{{ asset(mix('js/manifest.js')) }}" defer></script>
     <script src="{{ asset(mix('js/vendor.js')) }}" defer></script>
@@ -13,5 +14,4 @@
 <body>
     @inertia
 </body>
-
 </html>
